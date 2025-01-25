2f8a7ab48b7781fb98f5072c0fbf2e123ac34afb
Camden MacIver <camdenmac@icloud.com>
2025-01-25 00:08:39 -0500

    Made the bubbler guy do that bubble thing

--- double-bubble/src/bubble_character.gd
+++ double-bubble/src/bubble_character.gd
@@ -1,25 +1,35 @@
-extends CharacterBody2D
-
+extends RigidBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var SCAN_WIDTH = 10
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+@onready var center_ray = $RayCast2DCenter
+@onready var left_ray = $RayCast2DLeft
+@onready var right_ray = $RayCast2DRight
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:		
+	if Input.is_action_pressed("l_click"):
+		center_ray.target_position = get_global_mouse_position() - position
+		var norm = center_ray.target_position.normalized()
+		var mag = center_ray.target_position.length()
+		
+		left_ray.target_position = norm.rotated(deg_to_rad(-SCAN_WIDTH)) * mag
+		right_ray.target_position = norm.rotated(deg_to_rad(SCAN_WIDTH)) * mag
+		
+		var cool_ray = Vector2(0,0)
+		if center_ray.is_colliding():
+			cool_ray = center_ray.target_position
+		elif left_ray.is_colliding():
+			cool_ray = left_ray.target_position
+		elif right_ray.is_colliding():
+			cool_ray = right_ray.target_position
+			
+		apply_force(cool_ray.normalized() * 500) # make this scale on distance
+			
+		
+		
+	if Input.is_action_just_pressed("r_click"):
+		#update_mouse()
+		pass
+		
