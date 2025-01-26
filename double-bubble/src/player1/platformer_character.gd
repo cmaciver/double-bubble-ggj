d6d77fb6b27951d6c6be87d6fbd771af75d3b3e7
Sabrina <sabrinapjack@gmail.com>
2025-01-25 20:03:01 -0500

    cleaned up jumping

--- double-bubble/src/player1/platformer_character.gd
+++ double-bubble/src/player1/platformer_character.gd
@@ -1,13 +1,23 @@
 extends CharacterBody2D
 
+# movement speed
 const SPEED = 400.0
-var jump_velocity = -500.0
-signal bubble_touched
-signal fan_shot
+
+#default jump velocity @ start of jump
+const JUMP_DEFAULT = -500.0
+
+#jump velocity, updated as you jump
+var jump_velocity : float
+
+#is shift being held
 var is_aiming = false
+
+#direction of fan aim
 var aim_dir = Vector2()
-var jump_mult : float = 0.0
+
 var is_jumping: bool
+
+#window of time where you can heighten jump after bouncing off bubble
 var bounce_timer = 0.0
 
 @onready var wind : Area2D = $WindGun
@@ -19,18 +29,18 @@ func _physics_process(delta: float) -> void:
 	else:
 		is_jumping = false
 		
+	#if you're bouncing, count down on the bounce timer
 	if bounce_timer > 0.0:
 		bounce_timer -= 0.03;
 		if(bounce_timer <= 0.0):
 			bounce_timer = 0
 			is_jumping = false
-			print("no longer bouncing")
 		
-		
+	#if you pressed jump soon after bouncing off bubble
 	if(Input.is_action_just_pressed("platform_jump") and bounce_timer > 0.0):
 		is_jumping = true
 
-	# If you release jump set jumping to false.
+	# If you release jump, set jumping to false.
 	if(Input.is_action_just_released("platform_jump")):
 		is_jumping = false
 	
@@ -39,13 +49,16 @@ func _physics_process(delta: float) -> void:
 		is_jumping = true;
 		velocity.y = jump_velocity
 		
+	#otherwise if you're already jumping, increase velocity if you keep holding jump up to a maximum velocity
 	elif is_jumping and Input.is_action_pressed("platform_jump") and jump_velocity > -600:
 		jump_velocity *= 1.01;
 		velocity.y = jump_velocity
 		
+	#otherwise reset the velocity
 	elif !Input.is_action_pressed("platform_jump") and is_on_floor():
-		jump_velocity = -500
+		jump_velocity = JUMP_DEFAULT
 		
+	#make falling a bit faster than jumping
 	if velocity.y > 0:
 		velocity.y *= 1.01
 		
@@ -57,11 +70,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("platform_aim"):
 		is_aiming = false
 		wind.hide()
-		#fan()
 		
-	if is_aiming:
-		pass
-		#velocity.y = JUMP_VELOCITY
 	
 	# Aiming
 	if Input.is_action_pressed("platform_aim"):
@@ -82,11 +91,13 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	#collide with bubble and bounce accordinlgyd
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		if collision.get_collider().has_method("pop"):
 			collision.get_collider().pop()
 			if(position.y < collision.get_collider().position.y):
 				bounce_timer = .8;
-				jump_velocity = -500;
+				jump_velocity = JUMP_DEFAULT;
 				velocity.y = jump_velocity;
